1684819046ca16a1c1f389c3390cf61835227d5b
igorliotta <liottaigor@gmail.com>
2023-12-07 15:37:44 +0100

    Added: home.blade.php in views, and Route for home in web.php in routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Imposto una rotta che mi porti alla visualizzazione della vista home.blade.php
+Route::get('/home', function () {
+    $data = [
+        'title' => 'HelloWorld'
+    ];
+
+    return view('home', $data);
 });
